05c58791cea4df342e51cbf9cf4fe8b066f30bdd
aleksandrs12 <90208363+aleksandrs12@users.noreply.github.com>
2023-03-08 10:04:13 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var colors = [["rgb(214,138,47)", "brown", "yellow"]]
+
+var element = document.getElementById("myDiv");
+element.style.backgroundColor = colors[0][0];
+
+var btn1 = document.getElementById("btn1");
+btn1.value = colors[0][1];
+
+var btn2 = document.getElementById("btn2");
+btn2.value = colors[0][2];
